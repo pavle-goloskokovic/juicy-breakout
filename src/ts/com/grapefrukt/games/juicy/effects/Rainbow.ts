11860ce81fff87ed0bfeb8d3d1f674b84e55aefe
Pavle <pgoloskokovic@gmail.com>
2024-03-28 13:44:11 +0100

    sorted ball trail drawing

--- src/ts/com/grapefrukt/games/juicy/effects/Rainbow.ts
+++ src/ts/com/grapefrukt/games/juicy/effects/Rainbow.ts
@@ -7,17 +7,19 @@ class Segment {
 
 export class Rainbow extends Phaser.GameObjects.Graphics {
 
-    private _segments: Segment[] = [];
-    private _verts: number[] = [];
-    // private _indices: number[] = [];
+    private segments: Segment[] = [];
+    private verts: number[] = [];
+    // private indices: number[] = [];
 
     addSegment (x: number, y: number): void
     {
+        const segments = this.segments;
+
         let seg: Segment;
 
-        while (this._segments.length > Settings.EFFECT_BALL_TRAIL_LENGTH)
+        while (segments.length > Settings.EFFECT_BALL_TRAIL_LENGTH)
         {
-            seg = this._segments.shift();
+            seg = segments.shift();
         }
 
         if (!seg)
@@ -28,7 +30,7 @@ export class Rainbow extends Phaser.GameObjects.Graphics {
         seg.x = x;
         seg.y = y;
 
-        this._segments.push(seg);
+        segments.push(seg);
     }
 
     redrawSegments (offsetX = 0, offsetY = 0): void
@@ -37,64 +39,67 @@ export class Rainbow extends Phaser.GameObjects.Graphics {
 
         if (!Settings.EFFECT_BALL_TRAIL) { return; }
 
-        let s1: Segment;
-        let s2: Segment;
-        let vertIndex = 0;
-        let offset = 0;
-        let ang = 0;
-        let sin = 0;
-        let cos = 0;
+        const segments = this.segments;
+        const verts = this.verts;
 
-        if (this._verts.length !== (this._segments.length - 1) * 4)
+        if (verts.length !== (segments.length - 1) * 4)
         {
-            this._verts.length = 0;
+            verts.length = 0;
         }
 
-        for (let j = 0; j < this._segments.length; ++j)
+        let s2: Segment;
+        let vertIndex = 0;
+
+        for (let i = 0; i < segments.length; ++i)
         {
-            s1 = this._segments[j];
+            const s1 = segments[i];
+
             if (s2)
             {
-                ang = Math.atan2(s1.y - s2.y, s1.x - s2.x) + Math.PI / 2;
-                sin = Math.sin(ang);
-                cos = Math.cos(ang);
-                for (let i = 0; i < 2; ++i)
+                const ang = Math.atan2(s1.y - s2.y, s1.x - s2.x) + Math.PI / 2;
+                const sin = Math.sin(ang);
+                const cos = Math.cos(ang);
+
+                for (let j = 0; j < 2; ++j)
                 {
-                    offset = (-.5 + i) * 9.0;
+                    let offset = (-.5 + j) * 9.0;
 
                     if (Settings.EFFECT_BALL_TRAIL_SCALE)
                     {
-                        offset *= j / this._segments.length;
+                        offset *= i / segments.length;
                     }
 
-                    this._verts[vertIndex++] = s1.x + cos * offset - offsetX;
-                    this._verts[vertIndex++] = s1.y + sin * offset - offsetY;
+                    verts[vertIndex++] = s1.x + cos * offset - offsetX;
+                    verts[vertIndex++] = s1.y + sin * offset - offsetY;
                 }
             }
 
             s2 = s1;
         }
 
-        if (this._verts.length >= 8)
+        if (verts.length >= 8)
         {
             this.fillStyle(Settings.COLOR_TRAIL);
 
-            for (let k = 0; k < this._verts.length / 2; k++)
+            for (let k = 0; k < verts.length / 2; k++)
             {
-                /*this._indices[k * 6    ] = k * 2;
-                this._indices[k * 6 + 1] = k * 2 + 1;
-                this._indices[k * 6 + 2] = k * 2 + 2;
-                this._indices[k * 6 + 3] = k * 2 + 1;
-                this._indices[k * 6 + 4] = k * 2 + 2;
-                this._indices[k * 6 + 5] = k * 2 + 3;*/
+                /*const indices = this.indices;
+                indices[k * 6    ] = k * 2;
+                indices[k * 6 + 1] = k * 2 + 1;
+                indices[k * 6 + 2] = k * 2 + 2;
+                indices[k * 6 + 3] = k * 2 + 1;
+                indices[k * 6 + 4] = k * 2 + 2;
+                indices[k * 6 + 5] = k * 2 + 3;*/
 
                 this.fillTriangle(
-                    this._verts[k * 2    ],
-                    this._verts[k * 2 + 1],
-                    this._verts[k * 2 + 2],
-                    this._verts[k * 2 + 1],
-                    this._verts[k * 2 + 2],
-                    this._verts[k * 2 + 3]
+                    verts[(k * 2    ) * 2], verts[(k * 2    ) * 2 + 1],
+                    verts[(k * 2 + 1) * 2], verts[(k * 2 + 1) * 2 + 1],
+                    verts[(k * 2 + 2) * 2], verts[(k * 2 + 2) * 2 + 1]
+                );
+                this.fillTriangle(
+                    verts[(k * 2 + 1) * 2], verts[(k * 2 + 1) * 2 + 1],
+                    verts[(k * 2 + 2) * 2], verts[(k * 2 + 2) * 2 + 1],
+                    verts[(k * 2 + 3) * 2], verts[(k * 2 + 3) * 2 + 1]
                 );
             }
         }
